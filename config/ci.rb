abd9ceb73d425a4e005645e78bf6663e96667358
Trevor Turk <trevorturk@gmail.com>
2025-11-04 10:02:45 -0600

    add zeitwerk:check to bin/ci

--- config/ci.rb
+++ config/ci.rb
@@ -3,6 +3,8 @@
 CI.run do
   step "Setup", "bin/setup --skip-server"
 
+  step "Zeitwerk: Eager loading check", "bin/rails zeitwerk:check"
+
   step "Style: Ruby", "bin/rubocop"
 
   step "Security: Gem audit", "bin/bundler-audit"
